89f280e4628fcfbd97b283505bd353b7bbf91fbd
Radoslaw Szymkiewicz <szymkiewicz.radoslaw@gmail.com>
2025-08-26 15:56:21 +0200

    Sync with Medusa

--- packages/nocto/src/providers/providers.tsx
+++ packages/nocto/src/providers/providers.tsx
@@ -6,6 +6,7 @@ import { DashboardApp } from "../dashboard-app"
 import { ExtensionProvider } from "./extension-provider"
 import { I18nProvider } from "./i18n-provider"
 import { ThemeProvider } from "./theme-provider"
+import { FeatureFlagProvider } from "./feature-flag-provider"
 
 type ProvidersProps = PropsWithChildren<{
   api: DashboardApp["api"]
@@ -17,9 +18,11 @@ export const Providers = ({ api, children }: ProvidersProps) => {
       <ExtensionProvider api={api}>
         <HelmetProvider>
           <ThemeProvider>
-            <I18n />
-            <I18nProvider>{children}</I18nProvider>
-            <Toaster />
+            <FeatureFlagProvider>
+              <I18n />
+              <I18nProvider>{children}</I18nProvider>
+              <Toaster />
+            </FeatureFlagProvider>
           </ThemeProvider>
         </HelmetProvider>
       </ExtensionProvider>
